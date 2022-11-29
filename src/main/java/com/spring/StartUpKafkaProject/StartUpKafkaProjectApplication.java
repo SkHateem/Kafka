8121baa73fb518e7243fb021bef9f6a15b6645ef
Hateem Ahmed <hateem.ahmed@exazeit.com>
2022-11-29 11:54:08 +0530

    Kafka-Project

---
+++ src/main/java/com/spring/StartUpKafkaProject/StartUpKafkaProjectApplication.java
@@ -0,0 +1,13 @@
+package com.spring.StartUpKafkaProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StartUpKafkaProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StartUpKafkaProjectApplication.class, args);
+	}
+
+}
